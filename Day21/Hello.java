17c55dbd5b0b195697dd7ddf19e606d66bd328e4
Christian Hujer <Christian.Hujer@nelkinda.com>
2024-03-27 14:02:44 +0530

    Update Day21 content.

--- Day21/Hello.java
+++ Day21/Hello.java
@@ -1,9 +1,15 @@
+import java.util.Locale;
+
 public class Hello {
+    static {
+        Locale.setDefault(Locale.GERMANY);
+    }
     public static void main(final String... args) {
         System.out.println(getHelloMessage());
     }
     public static String getHelloMessage() {
-        if (System.getenv("LANG").equals("de")) return "Hallo, Welt!";
+        if (Locale.getDefault().getLanguage().equals("de"))
+            return "Hallo, Welt!";
         return "Hello, world!";
     }
 }
--- Day21/PrintLocale.java
+++ Day21/PrintLocale.java
@@ -2,6 +2,32 @@ import java.util.Locale;
 
 public class PrintLocale {
     public static void main(final String... args) {
-        System.out.println(Locale.getDefault());
+        var defaultLocale = Locale.getDefault();
+
+        //printLocaleInformation(Locale.US);
+        //printLocaleInformation(Locale.GERMANY);
+
+        //Locale.setDefault(Locale.GERMANY);
+
+        //printLocaleInformation(Locale.US);
+        //printLocaleInformation(Locale.GERMANY);
+
+        System.out.println(Locale.US.getDisplayCountry(Locale.FRANCE));
+        System.out.println(Locale.US.getDisplayName(Locale.FRANCE));
+    }
+    public static void printLocaleInformation(final Locale locale) {
+        System.out.println(locale);
+        System.out.format("country: %s%n", locale.getCountry());
+        System.out.format("display country: %s%n", locale.getDisplayCountry());
+        System.out.format("display language: %s%n", locale.getDisplayLanguage());
+        System.out.format("display name: %s%n", locale.getDisplayName());
+        System.out.format("display script: %s%n", locale.getDisplayScript());
+        System.out.format("display variant: %s%n", locale.getDisplayVariant());
+        System.out.format("ISO3 country: %s%n", locale.getISO3Country());
+        System.out.format("ISO3 language: %s%n", locale.getISO3Language());
+        System.out.format("language: %s%n", locale.getLanguage());
+        System.out.format("script: %s%n", locale.getScript());
+        System.out.format("variant: %s%n", locale.getVariant());
+        System.out.println();
     }
 }
